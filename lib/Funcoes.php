248729b62bbd9c8b94831f36bc348359ec234b0b
Elzo Brito dos Santos Filho <elzo.santos@cps.sp.gov.br>
2023-06-12 13:57:43 -0300

    Atualização da class Funções

--- lib/Funcoes.php
+++ lib/Funcoes.php
@@ -3,24 +3,31 @@
 namespace OliviaFrameLib;
 
 use DateTime;
+use OliviaCryptography\AESDecrypt;
+use OliviaCryptography\AESEncrypt;
 
-trait Funcoes
+class Funcoes
 {
-    public function setGetRequest($item)
+    private $parametros;
+    private $arrays;
+
+    public static function setGetRequest($item)
     {
-        $var[$item . '_cript'] = $this->requestGet($item);
-        $var[$item] = $this->decryptIt($var[$item . '_cript']);
+        $AESDecrypt = new AESDecrypt();
+        $var[$item . '_cript'] = self::requestGet($item);
+        $var[$item] = $AESDecrypt->decrypt($var[$item . '_cript'], $_SESSION['secret_key'], $_SESSION['secret_iv']);
         return $var;
     }
 
-    public function setPostRequest($item)
+    public static function setPostRequest($item)
     {
-        $var[$item . '_cript'] = $this->requestPost($item);
-        $var[$item] = $this->decryptIt($var[$item . '_cript']);
+        $AESDecrypt = new AESDecrypt();
+        $var[$item . '_cript'] = self::requestPost($item);
+        $var[$item] = $AESDecrypt->decrypt($var[$item . '_cript'], $_SESSION['secret_key'], $_SESSION['secret_iv']);
         return $var;
     }
 
-    public function findObject($wheres, $values, $array)
+    public static function findObject($wheres, $values, $array)
     {
         $arrayFinal = [];
 
@@ -42,7 +49,7 @@ trait Funcoes
         return $arrayFinal;
     }
 
-    public function capitalizeWords($string)
+    public static function capitalizeWords($string)
     {
         $arr = explode(' ', $string);
         foreach ($arr as $key => $word) {
@@ -53,7 +60,7 @@ trait Funcoes
         return implode(' ', $arr);
     }
 
-    public function getMonthName($month)
+    public static function getMonthName($month)
     {
         $months = [
             'Janeiro',
@@ -74,7 +81,7 @@ trait Funcoes
         return $months[$numero - 1] ?? '';
     }
 
-    public function removeAccents($string)
+    public static function removeAccents($string)
     {
         $search = ['á', 'à', 'ã', 'â', 'ä', 'Á', 'À', 'Ã', 'Â', 'Ä', 'é', 'è', 'ê', 'ë', 'É', 'È', 'Ê', 'Ë', 'í', 'ì', 'î', 'ï', 'Í', 'Ì', 'Î', 'Ï', 'ó', 'ò', 'õ', 'ô', 'ö', 'Ó', 'Ò', 'Õ', 'Ô', 'Ö', 'ú', 'ù', 'û', 'ü', 'Ú', 'Ù', 'Û', 'Ü', 'ñ', 'Ñ', 'ç', 'Ç'];
         $replace = ['a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A', 'A', 'e', 'e', 'e', 'e', 'E', 'E', 'E', 'E', 'i', 'i', 'i', 'i', 'I', 'I', 'I', 'I', 'o', 'o', 'o', 'o', 'o', 'O', 'O', 'O', 'O', 'O', 'u', 'u', 'u', 'u', 'U', 'U', 'U', 'U', 'n', 'N', 'c', 'C'];
@@ -82,7 +89,7 @@ trait Funcoes
         return str_replace($search, $replace, $string);
     }
 
-    public function formatDate($data, $includeDate = true, $includeTime = false)
+    public static function formatDate($data, $includeDate = true, $includeTime = false)
     {
         if ($data != null) {
             $dateTime = new DateTime($data);
@@ -93,62 +100,62 @@ trait Funcoes
         }
     }
 
-    public function getArrays(): array
+    public static function getArrays(): array
     {
-        return $this->arrays;
+        return self::$arrays;
     }
 
-    public function setArrays(array $arrays): void
+    public static function setArrays(array $arrays): void
     {
-        $this->arrays = $this->requestArrayPost($arrays);
+        self::$arrays = self::requestArrayPost($arrays);
     }
 
-    public function getAllParametros(): array
+    public static function getAllParametros(): array
     {
-        return $this->parametros;
+        return self::$parametros;
     }
 
-    public function getParametros($indice)
+    public static function getParametros($indice)
     {
-        return $this->parametros[$indice] ?? null;
+        return self::$parametros[$indice] ?? null;
     }
 
-    public function setParametros($nome = null): void
+    public static function setParametros($nome = null): void
     {
-        $this->parametros = $this->requestArrayPost($nome ?? 'params');
+        self::$parametros = self::requestArrayPost($nome ?? 'params');
     }
 
-    public function getDateTime($format): string
+    public static function getDateTime($format): string
     {
         return (new DateTime())->format($format);
     }
 
-    public function requestFile($dado)
+    public static function requestFile($dado)
     {
         return $_FILES[$dado] ?? null;
     }
 
-    public function requestArrayGet($dado): array
+    public static function requestArrayGet($dado): array
     {
         return (array) filter_input(INPUT_GET, $dado, FILTER_DEFAULT, FILTER_FORCE_ARRAY);
     }
 
-    public function requestArrayPost($dado): array
+    public static function requestArrayPost($dado): array
     {
         return (array) filter_input(INPUT_POST, $dado, FILTER_DEFAULT, FILTER_FORCE_ARRAY);
     }
 
-    public function requestPost($dado)
+    public static function requestPost($dado)
     {
         return filter_input(INPUT_POST, $dado);
     }
 
-    public function requestGet($dado)
+    public static function requestGet($dado)
     {
         return filter_input(INPUT_GET, $dado);
     }
 
-    public function decodeJwtToken($token)
+    public static function decodeJwtToken($token)
     {
         if (!empty($token)) {
             $parts = explode('.', $token);
